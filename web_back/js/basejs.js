1f6106e4edc3899b46c9fe539bcde6a7c415e365
Jiao Jian <903939365@qq.com>
2019-11-24 11:59:09 +0800

    文章类别管理

--- web_back/js/basejs.js
+++ web_back/js/basejs.js
@@ -16,5 +16,36 @@ var base = {
         $.get(jidi.yonghu, function(res) {
             aa(res);
         })
+    },
+    fabu: function(aa) {
+        $.post(jidi.fabu, { formData }, function() {
+
+        })
+    },
+    sousuo: function(aa) {
+        $.git(jidi.sousuo, function() {})
+    },
+    bianjiwenzhang: function(aa) {
+        $.post(jidi.bianjiwenzhang, function(res) {
+            aa(res);
+        })
+    },
+    leibei: function(aa) {
+        $.get(jidi.leibei, function(res) {
+            aa(res)
+        })
+    },
+    wenzhangshanchu: function(id, aa) {
+        $.post(jidi.wenzhangshanchu, { id: id }, function(res) {
+            aa(res);
+        })
+    },
+    bainjileibei: function(id, name, slug, aa) {
+        $.post(jidi.bainjileibei, { id: id, name: name, slug: slug }, function(res) {
+            aa(res)
+        })
+    },
+    xinzengwenzhang: function() {
+        $.post()
     }
 }
--- web_back/js/jidi.js
+++ web_back/js/jidi.js
@@ -3,4 +3,19 @@ var jidi = {
     login: url + "admin/login",
     tui: url + "admin/logout",
     yonghu: url + "admin/getuser",
+
+    // 文章发布
+    fabu: url + "admin/article_publish",
+    // 文章搜索
+    sousuo: url + "admin/search",
+    // 编辑文章
+    bianjiwenzhang: url + "admin/article_edit",
+    // 文章类别管理
+    leibei: url + "admin/category_search",
+    // 文章类别删除
+    wenzhangshanchu: url + "admin/category_delete",
+    // 编辑类别
+    bainjileibei: url + "admin/category_edit",
+    // 新增文章
+    xinzengwenzhang: url + "admin/category_add"
 }
